d3bd56a34481134aa2dc83226b3aa4329d0284ca
ssli3 <1273119600@qq.com>
2017-12-21 20:24:22 +0800

    第一个java测试程序

---
+++ src/javaTest/javaTest.java
@@ -0,0 +1,11 @@
+package javaTest;
+
+public class javaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("you are a handsome boy");
+	}
+
+}
